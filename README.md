7bee334116883a611636cf12c410baa9366e5106
mxue <admin@itmxue.cn>
2018-11-11 04:07:35 +0800

    优化迁移脚本，修改后台页面logo尺寸

--- README.md
+++ README.md
@@ -35,13 +35,13 @@ gogs：[https://gogs.itmxue.cn/mingxue/MKOnlinePlayer_Admin](https://gogs.itmxue
 
 **本教程以宝塔面板为例子进行讲解。**
 
-1. 本项目依赖于node.js 8.x，需要先下载node.js，宝塔面板用户可以直接在软件管理下载“pm2”，安装完成以后进入下一步。
+1. 本项目依赖于node.js 8.x和pm2，需要先下载node.js以及pm2，宝塔面板用户可以直接在软件管理下载“pm2”，安装完成以后进入下一步。
 2. 进入项目的github仓库（[https://github.com/mxue12138/MKOnlinePlayer_Admin](https://github.com/mxue12138/MKOnlinePlayer_Admin) ），点击右侧的“  Clone or download ”按钮，下载压缩包。
 3. 在服务器上新建一个网站，php版本默认或者选择纯静态都可以，然后选择反向代理，地址填``http://127.0.0.1:318``。
 4. 在服务器上任意一个文件夹内上传刚才下载的压缩包（宝塔用户建议上传到/www/wwwroot/下），然后解压。解压后会有一个MKOnlinePlayer_Admin_master文件夹，里面就是所有的代码了。
 5. 进入``MKOnlinePlayer_Admin_master/data/``下，打开``user.json``，里面的password后面的admin就是后台密码，修改下密码然后保存即可（不要删除两边的引号）。
 6. 使用SSH软件登录服务器（比如putty等），然后进入到你的项目目录下，比如宝塔用户就是/www/wwwroot/，那么执行命令`` cd /www/wwwroot/MKOnlinePlayer_Admin_master/``，就进入了程序目录。
-7. 进入到程序目录后执行``npm i``，执行完毕后执行``pm2 start bin/www``，执行完毕访问之前新建的那个网站就可以了，后台地址是admin。
+7. 进入到程序目录后执行``npm i && pm2 start bin/www``，执行完毕访问之前新建的那个网站就可以了，后台地址是admin。
 
 ## 注意事项
 
@@ -53,7 +53,8 @@ gogs：[https://gogs.itmxue.cn/mingxue/MKOnlinePlayer_Admin](https://gogs.itmxue
 6. 如果安装过程或者运行中有任何问题欢迎添加qq告诉我，没必要的问题不会回答的。
 7. 必须用pm2启动才可以，直接用node.js启动会出现修改后不生效的问题。
 8. 后台修改配置后需清除浏览器缓存才可以看到新内容，并不是没有修改成功。
-9. 本程序带有一个替换脚本，可以把你以前的歌单和player.js的配置批量替换到本程序内，可以使用项目目录下的``switch.js``进行批量转换，将你的``musicList.js``和``player.js``复制到当前项目目录下（也可以只复制一个），执行``node switch.js``没有报错即可，本脚本使用正则表达式查找批量替换。注意，本脚本不会将自定义歌单转换进项目内，如需要使用自定义歌单，请等待项目更新（手动复制进去也会被覆盖）。
+9. 本程序带有一个替换脚本，可以把你以前的musitList.js、player.js、index.html的内容批量替换到本程序内，可以使用项目目录下的``start.sh``进行批量转换，将你的``musicList.js``和``player.js``还有``index.html``复制到当前项目目录下（也可以只复制一个），然后执行``./start.sh``，等待他执行完成没有报错即可，本脚本使用正则表达式查找批量替换。
+10. 注意第九条的替换脚本不会将自定义歌单转换进项目内，如需要使用自定义歌单，请等待项目更新（手动复制进去也会被覆盖）。
 
 ## 演示地址
 
@@ -68,9 +69,8 @@ gogs：[https://gogs.itmxue.cn/mingxue/MKOnlinePlayer_Admin](https://gogs.itmxue
 ## 挖坑待填
 
 1. 添加自定义歌单功能。
-2. 优化界面。
-3. 添加自动更新按钮。
-4. 添加修改密码功能。
-5. 添加多用户功能，多个用户可以保存自己的歌单。
-6. 缓存歌曲记录（node.js是i/o密集型应用，读取缓存文件经测试可达php的10倍左右，勿喷，仅供参考）。
-7. 脱离php远程api（这个坑有点难）。
+2. 添加自动更新按钮（用户可选）。
+3. 添加修改密码功能。
+4. 添加多用户功能，多个用户可以保存自己的歌单。
+5. 缓存歌曲记录（node.js是i/o密集型应用，读取缓存文件经测试可达php的10倍左右，勿喷，仅供参考）。
+6. 脱离php远程api（这个坑有点难）。
--- switch.js
+++ switch.js
@@ -1,8 +1,7 @@
 let fs = require('fs');
 
 if (!fs.existsSync(process.cwd() + '/musicList.js')) {
-  console.log('musicList.js不存在，请确认文件是否已经复制到当前目录下。')
-  return;
+  console.log('musicList.js不存在，请确认文件是否已经复制到当前目录下。');
 } else {
   fs.readFile(process.cwd() + '/musicList.js', 'utf8', (err, data) => {
     if (err) {
@@ -25,21 +24,20 @@ if (!fs.existsSync(process.cwd() + '/musicList.js')) {
       }
     }
       let text = `/**************************************************
-   * MKOnlinePlayer v2.32
-   * 播放列表配置模块
-   * 编写：mengkun(http://mkblog.cn)
-   * 时间：2017-9-15
-  *************************************************/
-  // 建议修改前先备份一下
-  // 获取 歌曲的网易云音乐ID 或 网易云歌单ID 的方法：
-  // 先在 js/player.js 中开启调试模式，然后按 F12 打开浏览器的控制台。播放歌曲或点开歌单即可看到相应信息
+ * MKOnlinePlayer v2.32
+ * 播放列表配置模块
+ * 编写：mengkun(http://mkblog.cn)
+ * 时间：2017-9-15
+*************************************************/
+// 建议修改前先备份一下
+// 获取 歌曲的网易云音乐ID 或 网易云歌单ID 的方法：
+// 先在 js/player.js 中开启调试模式，然后按 F12 打开浏览器的控制台。播放歌曲或点开歌单即可看到相应信息
 
-  var musicList = ${ JSON.stringify(arr, null, 2)};
+var musicList = ${ JSON.stringify(arr, null, 2)};
 
-  if (typeof window == 'undefined') {
-    module.exports = musicList;
-  }`;
-    console.log(text);
+if (typeof window == 'undefined') {
+  module.exports = musicList;
+}`;
       fs.writeFile(process.cwd() + '/public/js/musicList.js', text, (err) => {
         if (err) {
           console.log(err);
@@ -52,8 +50,7 @@ if (!fs.existsSync(process.cwd() + '/musicList.js')) {
 }
 
 if (!fs.existsSync(process.cwd() + '/player.js')) {
-  console.log('player.js不存在，请确认文件是否已经复制到当前目录下。')
-  return;
+  console.log('player.js不存在，请确认文件是否已经复制到当前目录下。');
 } else {
   fs.readFile(process.cwd() + '/player.js', 'utf8', (err, data) => {
     if (err) {
@@ -68,7 +65,6 @@ if (!fs.existsSync(process.cwd() + '/player.js')) {
     let html = reg.exec(data)[1];
     let datas = eval('(' + html + ')');
     let text = JSON.stringify(datas, null, 2);
-    console.log(text);
     fs.writeFile(process.cwd() + '/data/player.json', text, (err) => {
       if (err) {
         console.log(err);
@@ -77,5 +73,36 @@ if (!fs.existsSync(process.cwd() + '/player.js')) {
       }
       console.log('player.json修改完毕，请删除本脚本（switch.js）以及当前目录下的player.js');
     });
-  })
+  });
+}
+
+if (!fs.existsSync(process.cwd() + '/index.html')) {
+  console.log('index.html不存在，请确认文件是否已经复制到当前目录下。');
+} else {
+  fs.readFile(process.cwd() + '/index.html', 'utf8', (err, data) => {
+    try {
+      let cheerio = require('cheerio');
+    } catch {
+      console.log('cheerio未安装');
+      return;
+    }
+    let $ = cheerio.load(data);
+    let datas = {
+      'title': $('title').text().trim(),
+      'description': $('meta[name="description"]').attr('content').trim(),
+      'keywords': $('meta[name="keywords"]').attr('content').trim(),
+      'logo_text': $('.header .logo').text().trim(),
+      'footer': $('body > span').text().trim(),
+      "notice_show": false,
+      "notice": "<p style=\"text-align: center;\">公告</p>"
+    }
+    fs.writeFile(process.cwd() + '/data/index.json', JSON.stringify(datas, null, 2), (err) => {
+      if (err) {
+        console.log(err);
+        console.log('修改失败，请确认index.html是否有写入权限');
+        return;
+      }
+      console.log('index.html修改完毕，请删除本脚本（switch.js）以及当前目录下的index.html');
+    });
+  });
 }
