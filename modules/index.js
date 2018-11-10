eea322cbe56ca146f5388c86ab851f792c3ee702
mxue <admin@itmxue.cn>
2018-11-11 06:53:59 +0800

    修复后台显示公告的问题

--- modules/index.js
+++ modules/index.js
@@ -1,7 +1,5 @@
 let index = require(process.cwd() + '/tools/index');
 
 module.exports = (req, res) => {
-  let data = index.read();
-  data.notice = data.notice.replace(/[\r\n]/g, '');
-  res.render('index', { data: data });
+  res.render('index', { data: index.read() });
 }
--- modules/player.js
+++ modules/player.js
@@ -1,7 +1,6 @@
 let player = require(process.cwd() + '/tools/player');
 
 module.exports = (req, res) => {
-  let data = player;
   res.set('Content-Type', 'application/javascript; charset=UTF-8');
-  res.render('player', { data: data });
+  res.render('player', { data: player.read() });
 }
