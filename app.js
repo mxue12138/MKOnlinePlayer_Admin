6fcc16ca4161306f3da3676d5c821adc231a4843
mxue <admin@itmxue.cn>
2018-11-11 22:28:25 +0800

    修复密码未修改也退出后台的bug，修复一个粗心的小bug

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var proxy = require('http-proxy-middleware');
 
 var app = express();
 
--- modules/admin/api/user.js
+++ modules/admin/api/user.js
@@ -4,6 +4,12 @@ let token = require(process.cwd() + '/tools/token');
 module.exports = (req, res) => {
   if(token(req)) {
     if (req.body.user) {
+      if (req.body.user.username == user.read().username && req.body.user.password == user.read().password) {
+        res.json({
+          code: 0,
+          msg: '账号或密码未修改'
+        });
+      }
       user.update(req.body.user);
       res.json({
         code: 1,
