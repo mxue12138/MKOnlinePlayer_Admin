a61623ad097e394a59b37afc15d85b9e1edb6fe2
mxue <admin@itmxue.cn>
2018-11-13 01:58:07 +0800

    忘记了加引号。。。

--- modules/download.js
+++ modules/download.js
@@ -57,10 +57,10 @@ module.exports = (req, res) => {
       request.get({
         url: url,
         headers: {
-          User-Agent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36',
-          Referer: protocol + host,
-          Origin: protocol + host,
-          Host: host
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36',
+          'Referer': protocol + host,
+          'Origin': protocol + host,
+          'Host': host
         }
       }).on('end', (end) => {
         res.json({
