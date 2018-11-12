678be4b74ccaa041526a312cab98b56d498b85fe
mxue <admin@itmxue.cn>
2018-11-13 02:30:38 +0800

    修改一个因为静态文件顺序引起的bug

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/temp', express.static(path.join(__dirname, 'temp'), {
   dotfiles: 'ignore',
   index: false,
@@ -23,6 +22,7 @@ app.use('/temp', express.static(path.join(__dirname, 'temp'), {
     res.setHeader('Content-Type', 'application/x-download');
   }
 }));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/index'));
 app.use('/admin', require('./routes/admin'));
