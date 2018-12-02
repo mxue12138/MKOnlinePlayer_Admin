67b93d716e15540775d5fdf65fa92e613f436e9d
mxue <admin@itmxue.cn>
2018-12-02 15:12:28 +0800

    修改歌单排序逻辑

--- modules/player.js
+++ modules/player.js
@@ -1,7 +1,7 @@
 let model = require(process.cwd() + '/model/model');
 
 module.exports = (req, res) => {
-  res.set('Content-Type', 'application/javascript; charset=UTF-8');
+  res.set('Content-Type', 'application/javascript');
   model.playerModel.findOne({}).select({ _id: 0 }).exec((err, playerData) => {
     res.render('player', { data: playerData });
   })
